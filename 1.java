2a07961fe4cdc5b18aa4262ad35cb57466ff6da0
Sowmith Prajwal <134672520+sowmithprajwal-18@users.noreply.github.com>
2024-11-05 19:56:17 +0530

    Update 1.java

--- 1.java
+++ 1.java
@@ -1,6 +1,7 @@
 public class Hi{
   public static void main(String args[]){
-      System.out.println("Sowmith prajwal");
+      System.out.println("hello VVCE");
+      System.out.println("Welcome to CSE department");
   }
 }
 
